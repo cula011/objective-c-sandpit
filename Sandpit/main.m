66a8ff80217ba94d2f2bd6d118e9ce8d44e1616b
Luka Kruscic <luka.kruscic@gmail.com>
2013-11-12 09:40:08 +1100

    Initial Commit

---
+++ Sandpit/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Sandpit
+//
+//  Created by Luka Kruscic on 12/11/2013.
+//  Copyright (c) 2013 Luka Kruscic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
